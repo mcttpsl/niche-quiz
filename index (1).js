4920f902d7431962315ba6f30b21b2afeab473f8
mcttpsl <mcttpsl@gmail.com>
2025-07-02 10:29:25 -0400

    Delete index (1).js

--- index (1).js
+++
@@ -1,4 +0,0 @@
-
-// Niche Quiz App with Google Sheets Integration
-// Data will be POSTed to: https://script.google.com/macros/s/AKfycbyl8Ar5tx4TSsfRhqkFe_PdN_Qm9xPYC8VK8_thbaLk0zq5q72xyK_lYMrwkWRF-vzX/exec
-console.log("JavaScript file ready for quiz with Google Sheets integration.");
